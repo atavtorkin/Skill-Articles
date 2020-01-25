d80a497f2532f97b7769a79b91d0428e36c3e4f4
atavtorkin <a.tavtorkin@apptimizm.pro>
2020-01-25 19:25:41 +0300

    General: hotfix

--- app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/data/repositories/ArticleRepository.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import ru.skillbranch.skillarticles.data.*
 
 
-object ArticleRepository {
+object  ArticleRepository {
     private val local = LocalDataHolder
     private val network = NetworkDataHolder
 
--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/ArticleViewModel.kt
@@ -22,7 +22,8 @@ class ArticleViewModel(private val articleId: String) :
                 title = article.title,
                 category = article.category,
                 categoryIcon = article.categoryIcon,
-                date = article.date.format()
+                date = article.date.format(),
+                author = article.author
             )
         }
 
